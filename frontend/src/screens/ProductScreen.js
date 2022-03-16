8fa82ffcd922d258fd1eeb6727fe52d25bdea90a
Stuckonyou98 <angelcoldplay5@gmail.com>
2022-03-16 17:51:44 +0630

    nodemon & concurrently

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 //NOTE https://stackoverflow.com/questions/70290770/react-typeerror-cannot-read-properties-of-undefined-reading-params
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({}) => {
   const { id } = useParams()
-  const product = products.find((p) => p._id === id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [id])
 
   return (
     <>
